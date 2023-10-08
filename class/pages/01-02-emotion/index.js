bca7a68e89eb296ea52d6a5bab1ccaf9b5e85727
he_hea <rnd903@gmail.com>
2023-10-08 19:25:00 +0900

    01-01

---
+++ class/pages/01-02-emotion/index.js
@@ -0,0 +1,11 @@
+import { Email, EmailInput } from "../../styles/emotion";
+
+export default function EmotionPage() {
+  return (
+    <>
+      <Email>이메일</Email>
+      <EmailInput type="text" />
+      <button>클릭하세요</button>
+    </>
+  );
+}
