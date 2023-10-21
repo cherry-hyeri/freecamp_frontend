77b0866fdf7f6942e3cb5833496632e476a90549
he_hea <rnd903@gmail.com>
2023-10-21 22:13:26 +0900

    portfolio3 done

--- freeboard_frontend/pages/boards2/new/index.js
+++ freeboard_frontend/pages/boards2/new/index.js
@@ -1,29 +1,39 @@
 import { useState } from "react";
 import {
-  Wrapper,
-  Title,
-  WriterWrapper,
+  Address,
+  ButtonWrapper,
+  Contents,
+  Error,
+  ImageWrapper,
   InputWrapper,
   Label,
-  Writer,
-  Subject,
-  Contents,
-  ZipcodeWrapper,
-  Zipcode,
-  SearchBtn,
-  Address,
-  Youtube,
-  ImageWrapper,
-  UploadBtn,
   OptionWrapper,
-  RadioBtn,
+  Password,
+  RadioButton,
   RadioLabel,
-  ButtonWrapper,
-  SubmitBtn,
-  Error,
-} from "../../../styles/emotion2";
+  SearchButton,
+  Subject,
+  SubmitButton,
+  Title,
+  UploadButton,
+  Wrapper,
+  Writer,
+  WriterWrapper,
+  Youtube,
+  Zipcode,
+  ZipcodeWrapper
+} from "../../../styles/emotion";
+import { gql, useMutation } from '@apollo/client'
 
-export default function BoardWritePage() {
+const CREATE_BOARD = gql`
+  mutation createBoard($createBoardInput: CreateBoardInput!){
+    createBoard(createBoardInput: $createBoardInput){
+      _id
+    }
+  }
+`
+
+export default function BoardWriteUI() {
   const [writer, setWriter] = useState("");
   const [password, setPassword] = useState("");
   const [title, setTitle] = useState("");
@@ -34,117 +44,117 @@ export default function BoardWritePage() {
   const [titleError, setTitleError] = useState("");
   const [contentsError, setContentsError] = useState("");
 
+  const [createBoard] = useMutation(CREATE_BOARD)
+
   const onChangeWriter = (event) => {
     setWriter(event.target.value);
-    if (event.target.value !== "") {
-      setWriterError("");
-    }
-  };
-  const onChangePassword = (event) => {
-    setPassword(event.target.value);
-    if (event.target.value !== "") {
-      setPasswordError("");
-    }
-  };
-  const onChangeTitle = (event) => {
-    setTitle(event.target.value);
-    if (event.target.value !== "") {
-      setTitleError("");
-    }
-  };
-  const onChangeContents = (event) => {
-    setContents(event.target.value);
-    if (event.target.value !== "") {
-      setContentsError("");
+    if(event.target.value !== ""){
+      setWriterError("")
     }
   };
 
-  const onClickSubmit = () => {
+  const onChangePassword = (event) => {
+    setPassword(event.target.value);
+    if(event.target.value !== ""){
+      setPasswordError("")
+    }
+  };
+
+  const onChangeTitle = (event) => {
+    setTitle(event.target.value);
+    if(event.target.value !== ""){
+      setTitleError("")
+    }
+  };
+
+  const onChangeContents = (event) => {
+    setContents(event.target.value);
+    if(event.target.value !== ""){
+      setContentsError("")
+    }
+  };
+
+  const onClickSubmit = async () => {
     if (!writer) {
-      setWriterError("작성자를 입력하세요.");
+      setWriterError("작성자를 입력해주세요.");
     }
     if (!password) {
-      setPasswordError("비밀번호를 입력하세요.");
+      setPasswordError("비밀번호를 입력해주세요.");
     }
     if (!title) {
-      setTitleError("제목을 입력하세요.");
+      setTitleError("제목을 입력해주세요.");
     }
     if (!contents) {
-      setContentsError("내용을 입력하세요");
+      setContentsError("내용을 입력해주세요.");
     }
     if (writer && password && title && contents) {
-      alert("게시글이 등록되었습니다.");
+      const result = await createBoard({
+        variables: {
+          createBoardInput: {
+            writer: writer,
+            password: password,
+            title: title,
+            contents: contents
+          }
+        }
+      })
+      console.log(result)
     }
   };
 
   return (
     <Wrapper>
-      <Title>게시물 등록</Title>
+      <Title>게시글 등록</Title>
       <WriterWrapper>
         <InputWrapper>
           <Label>작성자</Label>
-          <Writer
-            type="text"
-            placeholder="이름을 적어주세요."
-            onChange={onChangeWriter}
-          />
+          <Writer type="text" placeholder="이름을 적어주세요." onChange={onChangeWriter} />
           <Error>{writerError}</Error>
         </InputWrapper>
         <InputWrapper>
           <Label>비밀번호</Label>
-          <Writer
-            type="text"
-            placeholder="비밀번호를 입력해주세요."
-            onChange={onChangePassword}
-          />
+          <Password type="password" placeholder="비밀번호를 작성해주세요." onChange={onChangePassword} />
           <Error>{passwordError}</Error>
         </InputWrapper>
       </WriterWrapper>
       <InputWrapper>
         <Label>제목</Label>
-        <Subject
-          type="text"
-          placeholder="제목을 작성해주세요."
-          onChange={onChangeTitle}
-        />
+        <Subject type="text" placeholder="제목을 작성해주세요." onChange={onChangeTitle} />
         <Error>{titleError}</Error>
       </InputWrapper>
       <InputWrapper>
         <Label>내용</Label>
-        <Contents
-          placeholder="내용을 작성해주세요."
-          onChange={onChangeContents}
-        />
+        <Contents placeholder="내용을 작성해주세요." onChange={onChangeContents} />
         <Error>{contentsError}</Error>
       </InputWrapper>
       <InputWrapper>
         <Label>주소</Label>
         <ZipcodeWrapper>
-          <Zipcode type="number" />
-          <SearchBtn>우편번호 검색</SearchBtn>
+          <Zipcode placeholder="07250" />
+          <SearchButton>우편번호 검색</SearchButton>
         </ZipcodeWrapper>
-        <Address type="text" />
-        <Address type="text" />
+        <Address />
+        <Address />
       </InputWrapper>
       <InputWrapper>
         <Label>유튜브</Label>
-        <Youtube type="text" placeholder="링크를 복사해주세요." />
+        <Youtube placeholder="링크를 복사해주세요." />
       </InputWrapper>
       <ImageWrapper>
-        <Label>사진 첨부</Label>
-        <UploadBtn>+</UploadBtn>
-        <UploadBtn>+</UploadBtn>
-        <UploadBtn>+</UploadBtn>
+        <Label>사진첨부</Label>
+        <UploadButton>+</UploadButton>
+        <UploadButton>+</UploadButton>
+        <UploadButton>+</UploadButton>
       </ImageWrapper>
       <OptionWrapper>
-        <Label>메인 설정</Label>
-        <RadioBtn type="radio" id="youtube" name="radio-button" />
+        <Label>메인설정</Label>
+        <RadioButton type="radio" id="youtube" name="radio-button" />
         <RadioLabel htmlFor="youtube">유튜브</RadioLabel>
-        <RadioBtn type="radio" id="image" name="radio-button" />
+        <RadioButton type="radio" id="image" name="radio-button" />
         <RadioLabel htmlFor="image">사진</RadioLabel>
       </OptionWrapper>
       <ButtonWrapper>
-        <SubmitBtn onClick={onClickSubmit}>등록하기</SubmitBtn>
+        <SubmitButton onClick={onClickSubmit}>등록하기</SubmitButton>
       </ButtonWrapper>
     </Wrapper>
   );
